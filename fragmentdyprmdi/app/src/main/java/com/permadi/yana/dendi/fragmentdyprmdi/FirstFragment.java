cf01ac1ad61ef4f3d7f45f1bb7ac5a4649cf4610
Dendi Yana Permadi <81617834+dyprmadi@users.noreply.github.com>
2021-04-07 20:34:10 +0700

    Add files via upload

---
+++ fragmentdyprmdi/app/src/main/java/com/permadi/yana/dendi/fragmentdyprmdi/FirstFragment.java
@@ -0,0 +1,6 @@
+package com.permadi.yana.dendi.fragmentdyprmdi;
+
+import android.app.Fragment;
+
+public class FirstFragment extends Fragment {
+}
---
+++ fragmentdyprmdi/app/src/main/java/com/permadi/yana/dendi/fragmentdyprmdi/SecondActivty.java
@@ -0,0 +1,6 @@
+package com.permadi.yana.dendi.fragmentdyprmdi;
+
+import android.app.Activity;
+
+public class SecondActivty extends Activity {
+}
